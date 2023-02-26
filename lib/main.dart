e44d9f82843deab8daf50cc3a3ae78aff06f3895
Ahmad Einieh <66053897+ahmad-einieh@users.noreply.github.com>
2023-02-26 18:48:30 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,159 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:translator/translator.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainWidget(),
+    );
+  }
+}
+
+class MainWidget extends StatefulWidget {
+  const MainWidget({super.key});
+
+  @override
+  State<MainWidget> createState() => _MainWidgetState();
+}
+
+class _MainWidgetState extends State<MainWidget> {
+  String? _text;
+  int? pageCount;
+  int? currentPage;
+  bool isShow = true;
+  File? file;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isShow = true;
+                          file = null;
+                        });
+                      },
+                      icon: const Icon(Icons.close)),
+                  Text(
+                    _text == null ? "" : _text!.trim(),
+                    style: const TextStyle(fontSize: 20),
+                    locale: const Locale('ar'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) async {
+                        if (value.isNotEmpty) {
+                          final translator = GoogleTranslator();
+                          var t = await translator.translate(value,
+                              from: 'en', to: 'ar');
+                          setState(() {
+                            _text = t.text;
+                            _text = _text!.replaceAll(RegExp(r'\n+'), '');
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                  currentPage == null || pageCount == null
+                      ? const SizedBox()
+                      : SizedBox(
+                          width: 66,
+                          child: Text("$currentPage of $pageCount"),
+                        ),
+                ],
+              ),
+            ),
+            isShow
+                ? ElevatedButton(
+                    onPressed: () async {
+                      FilePickerResult? result =
+                          await FilePicker.platform.pickFiles();
+                      if (result != null) {
+                        setState(() {
+                          file = File(result.files.single.path!);
+                          isShow = false;
+                        });
+                      }
+                    },
+                    child: const Text("select file"))
+                : Container(),
+            !isShow
+                ? Expanded(
+                    child: SfPdfViewer.file(
+                      file!,
+                      pageLayoutMode: PdfPageLayoutMode.single,
+                      onDocumentLoaded: (details) {
+                        try {
+                          setState(() {
+                            pageCount = details.document.pages.count;
+                            currentPage = 1;
+                          });
+                        } catch (_) {}
+                      },
+                      onPageChanged: (details) {
+                        try {
+                          setState(() {
+                            currentPage = details.newPageNumber;
+                          });
+                        } catch (_) {}
+                        // try {
+                        //   final snackBar = SnackBar(
+                        //     content:
+                        //         Text("${details.newPageNumber} of $pageCount"),
+                        //     backgroundColor: (Colors.black12),
+                        //     duration: const Duration(milliseconds: 500),
+                        //   );
+                        //   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        // } catch (_) {}
+                      },
+                      onTextSelectionChanged: (details) async {
+                        if (details.selectedText != null) {
+                          try {
+                            final translator = GoogleTranslator();
+                            var t = await translator.translate(
+                                details.selectedText!,
+                                from: 'en',
+                                to: 'ar');
+                            setState(() {
+                              _text = t.text;
+                              _text = _text!.replaceAll(RegExp(r'\n+'), '');
+                            });
+                          } catch (_) {}
+                        }
+                      },
+                    ),
+                  )
+                : const SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
